a0644df78aebe6ce7cf668cd3c6dbfcc91dd48e2
Rajat Singhal <rajat0627@gmail.com>
2018-09-27 22:12:02 +0530

    signup

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const { User } = require('../models/User');
 const { QuizParticipation } = require('../models/QuizParticipation');
 
 const router = express.Router();
+
+router.post('/', async (req, res) => {
+    //TODO Validate with Joi
+    const user = new User({ name: req.body.name })
+    res.send(await user.save());
+})
 
 router.get('/me', async (req, res) => {
     res.send(await QuizParticipation.find());
