1e5387f5747ea724baeb6f5551433552e4bdc0f4
Rajat Singhal <rajat0627@gmail.com>
2018-09-28 18:54:50 +0530

    server_public_content

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use(express.json());
+router.use(express.static(__dirname+'/../public'));
 router.use('/api/users', require('./users'));
 router.get('/', (req, res) => res.send("Welcome to TVQ api!!") );
 
