b0f43a25fc5e7ace6669f59bd1c2a75c0af4a676
Rajat Singhal <rajat0627@gmail.com>
2018-10-02 23:43:57 +0530

    one_error_for_one_param

--- middlewares/validationErrors.js
+++ middlewares/validationErrors.js
@@ -2,10 +2,8 @@ const { validationResult } = require('express-validator/check');
 
 module.exports = function(req, res, next) {
     const errors = validationResult(req);
-    //TODO Write formatWith to send 1 error for 1 property
-    //const errors = validationResult(req).formatWith((errors)=>console.log(errors));
     if(!errors.isEmpty()) {
-        return res.status(422).json({ errors: errors.array() });
+        return res.status(422).json({ errors: errors.array({ onlyFirstError: true }) });
     }
     next();
 }
